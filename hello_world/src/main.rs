597dfe21e7eec01833a0d429ad0d671ac367d15b
devin <andy129408@gmail.com>
2019-11-22 22:58:41 +0800

    add print function

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,30 @@
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure);
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
 fn main() {
     println!("Hello, world!");
+    let x = 5 + /* 90 +  */ 5;
+    println!("`x` , x = {}", x);
+
+    println!("{0} this is {1}, {1} this is {0}", "Alice", "Bob");
+    println!("{developer}", developer = "LikeRust");
+
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+    // Pretty print
+    println!("{:#?}", peter);
 }
 // cd rust.learn
 // cargo new hello_world
--- hello_world/target/debug/deps/hello_world-43546f98d4d08171.d
+++ hello_world/target/debug/deps/hello_world-43546f98d4d08171.d
@@ -1,5 +1,5 @@
-G:\rust.learn\hello_world\target\debug\deps\hello_world-43546f98d4d08171.exe: src\main.rs
+E:\rust.learn\hello_world\target\debug\deps\hello_world-43546f98d4d08171.exe: src\main.rs
 
-G:\rust.learn\hello_world\target\debug\deps\hello_world-43546f98d4d08171.d: src\main.rs
+E:\rust.learn\hello_world\target\debug\deps\hello_world-43546f98d4d08171.d: src\main.rs
 
 src\main.rs:
