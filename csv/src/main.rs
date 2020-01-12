f7ee40b09da57b9295f752cc143ea551a8504db9
devin <andy129408@gmail.com>
2020-01-12 22:45:38 +0800

    读取csv

--- csv/src/main.rs
+++ csv/src/main.rs
@@ -1,14 +1,32 @@
-extern crate quick_csv;
+extern crate csv;
+extern crate rustc_serialize;
+use std::thread;
+use std::io;
+use std::time::{Duration, Instant};
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct smallpop {
+    city: String,
+    region: String,
+    country: String,
+    population: i32,
+}
+
 
 fn main() {
-    let data = "a,b\r\nc,d\r\ne,f";
-    let csv = quick_csv::Csv::from_string(data);
-    for row in csv.into_iter() {
-        // work on csv row ...
-        if let Ok(_) = row {
-            println!("new row!");
-        } else {
-            println!("cannot read next line");
-        }
+    let mut rdr =
+        csv::Reader::from_path("E:\\rust.learn\\csv\\data\\smallpop.csv").unwrap().has_headers();
+    let mut data: Vec<smallpop> = Vec::new();
+    for record in rdr.decode() {
+        let mut temp: smallpop = record.unwrap();
+        println!("len: =>{:?},{},{},{},{}",
+                 data.len(),
+                 temp.city,
+                 temp.region,
+                 temp.country,
+                 temp.population);
+        data.push(temp);
     }
+    thread::sleep_ms(500000);
+
 }
