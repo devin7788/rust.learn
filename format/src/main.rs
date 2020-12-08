43c82354b71bd3e25841b4ee31392e9d6538c7b2
devin7788 <andy129408@gmail.com>
2020-12-08 15:21:39 +0800

    rust学习

--- format/src/main.rs
+++ format/src/main.rs
@@ -1,5 +1,54 @@
+use redis::Commands;
+
+// 这个结构体不能使用 `fmt::Display` 或 `fmt::Debug` 来进行打印。
+struct UnPrintable(i32);
+
+// `derive` 属性会自动创建所需的实现，使这个 `struct` 能使用 `fmt::Debug` 打印。
+#[derive(Debug)]
+struct DebugPrintable(i32);
+
 fn main() {
     println!("Hello, world!");
-
     println!("{} days", 31);
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    // 可以使用命名参数。
+    println!("{subject} {verb} {object}",
+             object = "the lazy dog",
+             subject = "the quick brown fox",
+             verb = "jumps over");
+    // 可以在 `:` 后面指定特殊的格式。
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+    // 下面语句输出 "     1"，5 个空格后面连着 1。
+    println!("{number:>width$}", number = 1, width = 6);
+    // 100前面1个空格, 加起来4个字符宽度
+    println!("{number:>4}", number = 100);
+    // 你好: 世界
+    println!("你好，世界！");
+    // println! 会检查使用到的参数数量是否正确。
+    // println!("My name is {0}, {1} {0}", "Bond");
+
+    // #[allow(dead_code)]
+    // struct Structure(i32);
+
+    // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
+    // 下面语句无法运行。
+    // println!("This struct `{}` won't print...", Structure(3));
+    // 改正 ^ 注释掉此行。
+    // connect to redis
+
+    println!("{:?}", fetch_an_integer())
+}
+
+
+fn fetch_an_integer() -> redis::RedisResult<isize> {
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+
+    // throw away the result, just make sure it does not fail
+    let _ : () = con.set("my_key", 42)?;
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    con.get("my_key")
 }
